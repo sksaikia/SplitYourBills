184cd442e99fec369599e918d3beee64179d2fbf
Knightbarron <saikiasourav48@gmail.com>
2021-07-25 20:15:26 +0530

    Few changes

--- SplitYourBillsAndroid/app/src/main/java/com/example/splityourbillsandroid/utils/Constants.java
+++ SplitYourBillsAndroid/app/src/main/java/com/example/splityourbillsandroid/utils/Constants.java
@@ -3,4 +3,5 @@ package com.example.splityourbillsandroid.utils;
 public class Constants {
     public static final String PREF_FILE_NAME = "default";
     public static final String BUNDLE_ALL_TO_SINGLE_CAT = "all_to_single";
+    public static final String SPACE_ID = "";
 }
